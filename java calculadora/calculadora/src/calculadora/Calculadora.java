f5e0693a6ac9cc2c0a5ceb8115e964be32eb9700
Rodrigo Lopes <rodrigo.bleachex@hotmail.com>
2022-08-19 09:27:21 -0300

    first commit

---
+++ java calculadora/calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author kiro
+ */
+
+// a class principal tem que ter o nome do arquivo
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        soma(10,5);
+    }
+
+    public static void soma(int x, int y) {
+        System.out.println(x+y);
+    }
+    
+}
+ 
+/*
+public class seSenao{
+    public static void main (String[] args){
+     // não funciona
+    }
+}
+*/
